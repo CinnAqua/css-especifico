9661a81046f21823148366cf2aa6492ce0268481
CinnAqua <miguelcp1008@gmail.com>
2023-11-22 20:15:50 -0300

    Adicionei SASS e GULP

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps')
+
+function compilaSass () {
+    return gulp.src('./source/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build'))
+}
+
+exports.sass = compilaSass;
+exports.watch = function() {
+    gulp.watch('./source/main.scss', gulp.series(compilaSass))
+}
